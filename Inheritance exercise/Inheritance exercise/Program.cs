0e87a2c9c54f7b21d7bd25f036acfdedcee78ba2
Amber Cook <30010451@student.toiohomai.ac.nz>
2019-03-20 15:58:13 +1300

    need to complete task 2

---
+++ Inheritance exercise/Inheritance exercise/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance_exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    class Person
+    {
+        public string Fname { get; set; }
+        public string Lname { get; set; }
+    }
+
+    class Student : Person
+    {
+        public int StudentId { get; set; }
+    }
+
+    class Employee : Person
+    {
+        public int EmployeeId { get; set; }
+    }
+
+    class Teacher : Employee
+    {
+        public string Office { get; set; }
+
+        public Teacher(string _fname, string _lname, string _empid, string _office )
+        {
+
+        }
+
+        public string GetTeachDeets()
+        {
+            return $"Firstname: {Fname} \nLastname: {Lname} \n EmployeeID: {EmployeeId} \nOffice: {Office} ";
+        }
+    }
+}
